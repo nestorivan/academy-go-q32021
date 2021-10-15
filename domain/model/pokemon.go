f0be7ce4c6e31789c6a0cda6b59d8f94a97d1cf4
Nestor Lopez <nlopezdev@gmail.com>
2021-10-14 18:43:22 -0700

    Add full list of pokemons to csv
    change pokemon model

--- domain/model/pokemon.go
+++ domain/model/pokemon.go
@@ -1,7 +1,17 @@
 package model
 
 type Pokemon struct {
-  Id string
+  Id int
   Name string
+  Type1 string
+  Type2 string `csv:"Type2,omitempty"`
+  Total int
+  HP int
+  Attack int
+  Defense int
+  SpAtk int
+  SpDef int
+  Generation int
+  Legendary bool
 }
 
